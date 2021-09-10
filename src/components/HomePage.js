8dc9c05995a01d354efb49479648b4c71b9ef574
Othman2001 <othman-dev@outlook.com>
2021-09-11 00:59:32 +0200

    adding perofmance optmiaztion 4

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,7 +11,7 @@ function HomePage(){
            <div className="home__main">
                <main className="home__grid">
                    <div className="home_part1">
-                   <motion.h1
+                   <h1
                    animate = {{  scale: [1, 2, 2, 1, 1],
                     rotate: [0, 0, 270, 270, 0],
                     borderRadius: ["20%", "20%", "50%", "50%", "20%"],}}
@@ -20,7 +20,7 @@ function HomePage(){
                    I'm  <span className= "span"> Othman </span>
                    <br />
                    Front   <span className= "span">  end developer </span> 
-                    </motion.h1>
+                    </h1>
                    <p className = "home__main__text">
                    Front-End-Developer / ReactJS </p>
                    <Button variant="outlined" className="home__button">
